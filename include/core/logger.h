862fd388574d5e9eadcd8574a4dd98d367f2a18e
antsoh161 <soderlund.anton@gmail.com>
2023-12-18 18:49:52 +0100

    Add primitive async logger

---
+++ include/core/logger.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <chrono>
+#include <condition_variable>
+#include <queue>
+#include <thread>
+#include <format>
+
+namespace logger {
+
+enum LogMedia {
+  LOG_File,
+  LOG_Console,
+  LOG_StdOut,
+};
+
+enum LogLevel {
+  NONE,
+  DEBUG,
+  INFO,
+  WARN,
+  ERROR,
+  FATAL,
+};
+
+
+struct LogMessage {
+  std::chrono::system_clock::time_point timestamp;
+  LogLevel level{NONE};
+  std::string message;
+};
+
+class AsyncLogger {
+ public:
+  AsyncLogger();
+  virtual ~AsyncLogger();
+  AsyncLogger(AsyncLogger&&) = delete;
+  AsyncLogger(const AsyncLogger&) = delete;
+  AsyncLogger& operator=(AsyncLogger&&) = delete;
+  AsyncLogger& operator=(const AsyncLogger&) = delete;
+
+  void start();
+  void stop();
+  void set_log_media(LogMedia media);
+
+  template <typename... Args>
+  void log(const std::format_string<Args...> fmt, Args&&... args) {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    std::string msg = std::format(fmt, std::forward<Args>(args)...);
+    m_shared_queue.push({std::chrono::system_clock::now(), LogLevel::INFO, msg});
+    m_cv.notify_one();
+  }
+
+ private:
+  void log_worker();
+
+  std::optional<LogMedia> m_log_media{LOG_StdOut};
+  bool m_enabled{false};
+  
+
+  std::jthread m_worker_thread;
+  std::queue<LogMessage> m_shared_queue;
+  std::mutex m_mutex;
+  std::condition_variable m_cv;
+};
+}  // namespace logger
---
+++ src/core/logger.cpp
@@ -0,0 +1,59 @@
+#include "core/logger.h"
+
+#include <iostream>
+
+#include "core/asserts.h"
+
+namespace logger {
+
+AsyncLogger::AsyncLogger() = default;
+
+AsyncLogger::~AsyncLogger() {
+  m_enabled = false;
+}
+
+void AsyncLogger::start() {
+  if (!m_log_media.has_value()) {
+    log("No log media selected, defaulting to stdout");
+    m_log_media = LogMedia::LOG_StdOut;
+  }
+
+  m_enabled = true;
+  m_worker_thread = std::jthread(&AsyncLogger::log_worker, this);
+}
+
+void AsyncLogger::stop() {
+  m_enabled = false;
+}
+
+void AsyncLogger::set_log_media(LogMedia media) {
+  m_log_media = media;
+}
+
+void AsyncLogger::log_worker() {
+  while (m_enabled) {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    m_cv.wait(lock, [this]() {
+      return !m_shared_queue.empty() || !m_enabled;
+    });
+
+    while (!m_shared_queue.empty()) {
+      auto msg = m_shared_queue.front();
+      m_shared_queue.pop();
+
+      // Do writes here.. Spawn individual writers...?
+      switch (m_log_media.value()) {
+        case LOG_StdOut:
+          std::cout << "[" << msg.timestamp << "] " << msg.message << "\n";
+          break;
+        case LOG_File:
+          break;
+        case LOG_Console:
+          break;
+        default:
+          M_ASSERT_U("Unknown Log media, abort");
+      }
+    }
+  }
+}
+}  // namespace logger
