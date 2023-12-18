81ed22cbc54c21ffa7419f30316f3dd3ed59fb03
antsoh161 <soderlund.anton@gmail.com>
2023-12-18 18:49:11 +0100

    Format and abort on asserts

--- include/core/asserts.h
+++ include/core/asserts.h
@@ -1,27 +1,28 @@
+#pragma once
+
 #include <format>
 #include <iostream>
 #include <stdexcept>
 
 #define M_ASSERT(expr, ...)                                                    \
-  if (expr) {                                                                  \
-  } else {                                                                     \
-    std::cerr << std::format("assertion failed: {} at {}:{}", #expr, __FILE__, \
-                             __LINE__, std::format(__VA_ARGS__))               \
-              << std::endl;                                                    \
-    throw std::runtime_error("assertion failed");                              \
-  }
+    if (expr) {                                                                \
+    } else {                                                                   \
+        std::cerr << std::format("assertion failed: {} at {}:{}", #expr,       \
+                                 __FILE__, __LINE__, std::format(__VA_ARGS__)) \
+                  << std::endl;                                                \
+        std::abort();                                                          \
+    }
 
-#define M_ASSERT_U(...)                                                  \
-  std::cerr << std::format("unconditional assertion failed: at {}:{}",   \
-                           __FILE__, __LINE__, std::format(__VA_ARGS__)) \
-            << std::endl;                                                \
-  throw std::runtime_error("Assertion failed");
+#define M_ASSERT_U(...)                                                    \
+    std::cerr << std::format("unconditional assertion failed: at {}:{}, {}",   \
+                             __FILE__, __LINE__, std::format(__VA_ARGS__)) \
+              << std::endl;
 
-#define M_ASSERT_NOTNULL(expr) \
-  if (expr != nullptr) {                                                     \
-  } else {                                                                   \
-    std::cerr << std::format("nullptr assertion failed: {} at {}:{}", #expr, \
-                             __FILE__, __LINE__)   \
-              << std::endl;                                                  \
-    throw std::runtime_error("assertion failed");                            \
-  }
+#define M_ASSERT_NOTNULL(expr)                                            \
+    if (expr != nullptr) {                                                \
+    } else {                                                              \
+        std::cerr << std::format("nullptr assertion failed: {} at {}:{}", \
+                                 #expr, __FILE__, __LINE__)               \
+                  << std::endl;                                           \
+        std::abort();                                                     \
+    }
