307b35b2d8633fe457cea6d8fcafedf4090c40e4
antsoh161 <soderlund.anton@gmail.com>
2024-02-04 16:00:42 +0100

    Meddl is now a library, as it should be.
    Examples and tests are also separated into where they should be.

---
+++ examples/main.cpp
@@ -0,0 +1,27 @@
+#include "core/asserts.h"
+#include "vulkan_renderer/context.h"
+#include "wrappers/glfw/window.h"
+
+int main() {
+   // Engine engine;
+   constexpr uint32_t WINDOW_WIDTH = 800;
+   constexpr uint32_t WINDOW_HEIGHT = 600;
+
+   glfwInit();
+   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+   glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+   auto window = std::make_shared<meddl::glfw::Window>(WINDOW_WIDTH, WINDOW_HEIGHT, "Meddl Test");
+
+   try {
+      auto ctx = meddl::vulkan::ContextBuilder()
+                     .window(window)
+                     .enable_debugger()
+                     .with_debug_layers({"VK_LAYER_KHRONOS_validation"})
+                     .build();
+   } catch (const std::exception& e) {
+      M_ASSERT_U("Initialization caught exception: {}", e.what())
+   }
+   // engine.run();
+   return EXIT_SUCCESS;
+}
