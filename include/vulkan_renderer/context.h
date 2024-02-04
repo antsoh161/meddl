078ebb684dd35ae356a6b45c0dd207087d9035a7
antsoh161 <soderlund.anton@gmail.com>
2024-02-04 15:59:40 +0100

    Add Context and ContextBuilder
    This makes it a lot easier to construct the exact kind of application
    that the user wants.

---
+++ include/vulkan_renderer/context.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <vulkan/vulkan_core.h>
+
+#include <optional>
+#include <set>
+#include <vector>
+
+#include "GLFW/glfw3.h"
+#include "vulkan_renderer/device.h"
+#include "vulkan_renderer/vulkan_debug.h"
+#include "wrappers/glfw/window.h"
+
+namespace meddl::vulkan {
+
+class Context {
+  public:
+   Context(std::shared_ptr<glfw::Window>&& window,
+           std::optional<VulkanDebugger>&& debugger,
+           VkDebugUtilsMessengerCreateInfoEXT debug_info,
+           VkApplicationInfo app_info,
+           const std::set<PhysicalDeviceRequirements>& pdr);
+
+   Context(const Context&) = delete;
+   Context& operator=(const Context&) = delete;
+
+  // TODO: this ok?
+   Context(Context&& other) noexcept = default;
+   Context& operator=(Context&& other) noexcept = default;
+
+   ~Context();
+
+  private:
+   void clean_up();
+   VkResult make_instance(const VkApplicationInfo& app_info,
+                          const VkDebugUtilsMessengerCreateInfoEXT& debug_info);
+   bool make_physical_devices();
+   bool pick_physical_device(const std::set<PhysicalDeviceRequirements>& pdr);
+   bool make_logical_device();
+
+   // TODO: Should a vulkan context hold this?
+   std::shared_ptr<glfw::Window> _window;
+   VkInstance _instance{VK_NULL_HANDLE};
+   VkSurfaceKHR _surface{VK_NULL_HANDLE};
+   std::vector<PhysicalDevice> _available_devices{};
+   PhysicalDevice* _active_device{nullptr};  // This just points to an element in the device vector
+   LogicalDevice _logical_device;
+   std::optional<VulkanDebugger> _debugger{};
+};
+
+// TODO: Move to meddl::vulkan::debug ?
+namespace{
+VKAPI_ATTR VkBool32 VKAPI_CALL
+meddl_debug_callback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                       VkDebugUtilsMessageTypeFlagsEXT messageType,
+                       const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+                       void* pUserData) {
+   std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+   return VK_FALSE;
+}
+}
+
+class ContextBuilder {
+  public:
+   ContextBuilder& window(std::shared_ptr<glfw::Window> window);
+   ContextBuilder& enable_debugger();
+   ContextBuilder& with_debug_layers(const std::vector<std::string>& layers);
+   ContextBuilder& with_debug_create_info(const VkDebugUtilsMessengerCreateInfoEXT& debug_info);
+   ContextBuilder& with_app_info(const VkApplicationInfo& app_info);
+   ContextBuilder& with_physical_device_requirements(const PhysicalDeviceRequirements& pdr);
+   ContextBuilder& with_physical_device_requirements(
+       const std::set<PhysicalDeviceRequirements>& pdr);
+   Context build();
+
+  private:
+   // TODO: Will this ptr be thread safe?
+   std::shared_ptr<glfw::Window> _window;
+   VkApplicationInfo _app_info{VK_STRUCTURE_TYPE_APPLICATION_INFO,
+                               nullptr,
+                               "Default Meddl Application",
+                               VK_MAKE_VERSION(1, 0, 0),
+                               "No engine",
+                               VK_MAKE_VERSION(1, 0, 0),
+                               VK_API_VERSION_1_0};
+   VkDebugUtilsMessengerCreateInfoEXT _debug_info{
+       VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+       nullptr,
+       0,
+       VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+           VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+           VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
+       VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+           VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+           VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
+       meddl_debug_callback,
+       nullptr,
+   };
+   std::set<PhysicalDeviceRequirements> _pdr{PhysicalDeviceRequirements::PD_GRAPHICS,
+                                             PhysicalDeviceRequirements::PD_PRESENT};
+   std::vector<std::string> _debug_layers{};
+
+   std::optional<VulkanDebugger> _debugger;
+};
+}  // namespace meddl::vulkan
---
+++ src/vulkan_renderer/context.cpp
@@ -0,0 +1,232 @@
+#include "vulkan_renderer/context.h"
+
+#include <vulkan/vulkan_core.h>
+
+#include <algorithm>
+#include <memory>
+#include <span>
+#include <utility>
+
+#include "core/asserts.h"
+#include "vulkan_renderer/device.h"
+
+namespace meddl::vulkan {
+
+Context::Context(std::shared_ptr<glfw::Window>&& window,
+                 std::optional<VulkanDebugger>&& debugger,
+                 VkDebugUtilsMessengerCreateInfoEXT debug_info,
+                 VkApplicationInfo app_info,
+                 const std::set<PhysicalDeviceRequirements>& pdr)
+    : _window(std::move(window)), _debugger(std::move(debugger)) {
+   if (make_instance(app_info, debug_info) != VK_SUCCESS) {
+      clean_up();
+      throw std::runtime_error("Vulkan instance creation failed");
+   }
+
+   if (_debugger.has_value()) {
+      auto func = reinterpret_cast<PFN_vkCreateDebugUtilsMessengerEXT>(         // NOLINT
+          vkGetInstanceProcAddr(_instance, "vkCreateDebugUtilsMessengerEXT"));  // NOLINT
+      // TODO: raw pointer?
+      if (func(_instance, &debug_info, nullptr, _debugger->get_messenger()) != VK_SUCCESS) {
+         clean_up();
+         throw std::runtime_error("Debug messenger creation failed");
+      }
+   }
+
+   if (glfwCreateWindowSurface(_instance, *_window, nullptr, &_surface) != VK_SUCCESS) {
+      clean_up();
+      throw std::runtime_error("Surface creation failed");
+   }
+
+   if (!make_physical_devices()) {
+      clean_up();
+      throw std::runtime_error("No vulkan compatible GPUs found");
+   }
+
+   if (!pick_physical_device(pdr)) {
+      clean_up();
+      throw std::runtime_error("No GPU has the requirements passed to context builder");
+   }
+   if (!make_logical_device()) {
+      clean_up();
+      throw std::runtime_error("Logical device creation failed");
+   }
+}
+
+Context::~Context() {
+   clean_up();
+}
+
+void Context::clean_up() {
+   // TODO: Order?
+   vkDestroyDevice(_logical_device, nullptr);
+   vkDestroySurfaceKHR(_instance, _surface, nullptr);
+   vkDestroyInstance(_instance, nullptr);
+}
+
+
+VkResult Context::make_instance(const VkApplicationInfo& app_info,
+                                const VkDebugUtilsMessengerCreateInfoEXT& debug_info) {
+   VkInstanceCreateInfo instance_create_info{};
+   instance_create_info.sType =
+       VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;  // TODO: can there be others?
+   instance_create_info.pApplicationInfo = &app_info;
+
+   uint32_t glfw_ext_count = 0;
+   auto glfw_ext = glfwGetRequiredInstanceExtensions(&glfw_ext_count);
+   auto ext_span = std::span(glfw_ext, glfw_ext_count);
+   std::vector<const char*> extensions(ext_span.begin(), ext_span.end());
+
+   std::vector<const char*> layers_cstyle{};
+   if (_debugger.has_value()) {
+      extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+      instance_create_info.enabledLayerCount = _debugger->get_active_validation_layers().size();
+
+      const auto& layers = _debugger->get_active_validation_layers();
+      for (const auto& layer : layers) {
+         layers_cstyle.push_back(layer.c_str());
+      }
+      // instance_create_info.ppEnabledLayerNames = layers_cstyle.data();
+      instance_create_info.ppEnabledLayerNames = layers_cstyle.data();
+      instance_create_info.pNext = &debug_info;
+   } else {
+      instance_create_info.enabledLayerCount = 0;
+      instance_create_info.pNext = nullptr;
+   }
+   instance_create_info.enabledExtensionCount = extensions.size();
+   instance_create_info.ppEnabledExtensionNames = extensions.data();
+
+   return vkCreateInstance(&instance_create_info, nullptr, &_instance);
+}
+
+bool Context::make_physical_devices() {
+   uint32_t n_devices = 0;
+   vkEnumeratePhysicalDevices(_instance, &n_devices, nullptr);
+   if (n_devices < 1) return false;
+
+   std::vector<VkPhysicalDevice> devices(n_devices);
+   vkEnumeratePhysicalDevices(_instance, &n_devices, devices.data());
+
+   for (const auto& device : devices) {
+      _available_devices.emplace_back(device, _surface);
+   }
+   return true;
+}
+
+bool Context::pick_physical_device(const std::set<PhysicalDeviceRequirements>& pdr) {
+   std::vector<PhysicalDevice*> candidates;
+   for (auto& device : _available_devices) {
+      if (device.fulfills_requirement(pdr)) {
+         candidates.push_back(&device);
+      }
+   }
+   if (candidates.size() > 0) {
+      _active_device = candidates[0];
+      return true;
+   }
+   return false;
+}
+
+
+bool Context::make_logical_device() {
+   // TODO: This might also be an initialization option
+   float queue_prio = 1.0f;
+   auto make_cinfo = [&queue_prio](unsigned int index) -> VkDeviceQueueCreateInfo {
+      VkDeviceQueueCreateInfo queue_cinfo{};
+      queue_cinfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+      queue_cinfo.queueFamilyIndex = index;
+      queue_cinfo.queueCount = 1;
+      queue_cinfo.pQueuePriorities = &queue_prio;
+      return queue_cinfo;
+   };
+
+   std::vector<VkDeviceQueueCreateInfo> queue_create_infos;
+   for (const auto& active_family : _active_device->get_pdr_to_index_map()) {
+      queue_create_infos.push_back(make_cinfo(active_family.second));
+   }
+
+   VkPhysicalDeviceFeatures features{};
+   VkDeviceCreateInfo create_info{};
+   create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+   create_info.queueCreateInfoCount = queue_create_infos.size();
+   create_info.pQueueCreateInfos = queue_create_infos.data();
+   create_info.pEnabledFeatures = &features;
+   create_info.enabledExtensionCount = 0;
+
+   std::vector<const char*> layers_cstyle{};
+   if (_debugger.has_value()) {
+      create_info.enabledLayerCount = _debugger->get_active_validation_layers().size();
+      const auto& layers = _debugger->get_active_validation_layers();
+      for (const auto& layer : layers) {
+         layers_cstyle.push_back(layer.c_str());
+      }
+      create_info.ppEnabledLayerNames = layers_cstyle.data();
+   } else {
+      create_info.enabledLayerCount = 0;
+   }
+
+   VkDevice handle{VK_NULL_HANDLE};  // TODO: Should LogicalDevice have operator VkDevice*()?
+   QueueFamilies queues{};
+   if (vkCreateDevice(*_active_device, &create_info, nullptr, &handle) != VK_SUCCESS) {
+      return false;
+   }
+   _logical_device = LogicalDevice(handle, queues);
+   for (const auto& active_family : _active_device->get_pdr_to_index_map()) {
+      vkGetDeviceQueue(_logical_device,
+                       active_family.second,
+                       0,
+                       &_logical_device.get_queue(active_family.first));
+   }
+   return true;
+}
+
+ContextBuilder& ContextBuilder::window(std::shared_ptr<glfw::Window> window) {
+   _window = window;
+   return *this;
+}
+
+ContextBuilder& ContextBuilder::enable_debugger() {
+   _debugger = VulkanDebugger();
+   return *this;
+}
+
+const std::vector<const char*> mega_layers{"VK_LAYER_KHRONOS_validation"};
+
+ContextBuilder& ContextBuilder::with_debug_layers(const std::vector<std::string>& layers) {
+   _debug_layers = layers;
+   return *this;
+}
+
+ContextBuilder& ContextBuilder::with_debug_create_info(
+    const VkDebugUtilsMessengerCreateInfoEXT& debug_info) {
+   _debug_info = debug_info;
+   return *this;
+}
+
+ContextBuilder& ContextBuilder::with_app_info(const VkApplicationInfo& app_info) {
+   _app_info = app_info;
+   return *this;
+}
+
+ContextBuilder& ContextBuilder::with_physical_device_requirements(
+    const std::set<PhysicalDeviceRequirements>& pdr) {
+   _pdr = pdr;
+   return *this;
+}
+
+ContextBuilder& ContextBuilder::with_physical_device_requirements(
+    const PhysicalDeviceRequirements& pdr) {
+   _pdr.insert(pdr);
+   return *this;
+}
+
+Context ContextBuilder::build() {
+   M_ASSERT(_window, "Can NOT create a vulkan context without a valid window");
+   if (_debugger.has_value()) {
+      _debugger->add_validation_layers(_debug_layers);
+      for(const auto& layer : _debugger->get_active_validation_layers())
+        std::cout << "Active layer in _Debugger: " << layer << "\n";
+   }
+   return {std::move(_window), std::move(_debugger), _debug_info, _app_info, _pdr};
+}
+}  // namespace meddl::vulkan
