b30aa25c6744eac38e6f13bca0614a0d040f689c
antsoh161 <soderlund.anton@gmail.com>
2024-03-17 02:40:36 +0100

    feat: Add Surface wrapper

---
+++ include/vk/surface.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "GLFW/glfw3.h"
+#include "vk/instance.h"
+#include "wrappers/glfw/window.h"
+
+namespace meddl::vk {
+class Surface {
+  public:
+   Surface(glfw::Window* window, Instance* instance);
+   ~Surface();
+
+   Surface(const Surface&) = delete;
+   Surface& operator=(const Surface&) = delete;
+
+   Surface(Surface&&) = default;
+   Surface& operator=(Surface&&) = default;
+
+   operator VkSurfaceKHR() const { return _surface; }
+   [[nodiscard]] VkSurfaceKHR vk() const { return _surface; }
+
+  private:
+   VkSurfaceKHR _surface{VK_NULL_HANDLE};
+   Instance* _instance;
+};
+
+}  // namespace meddl::vk
