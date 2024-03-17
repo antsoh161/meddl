3c64538ad67789f4137d3a115d1ef4026793f3e1
antsoh161 <soderlund.anton@gmail.com>
2024-03-17 02:41:01 +0100

    feat: Add instance wrapper

---
+++ include/vk/instance.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+#include <optional>
+
+#include "GLFW/glfw3.h"
+#include "vk/debug.h"
+#include "vk/defaults.h"
+#include "vk/device.h"
+
+namespace meddl::vk {
+class PhysicalDevice;
+class Instance {
+  public:
+   Instance(VkApplicationInfo app_info,
+            VkDebugUtilsMessengerCreateInfoEXT debug_info,
+            std::optional<Debugger>& debugger);
+   ~Instance();
+   Instance(const Instance&) = delete;
+   Instance& operator=(const Instance&) = delete;
+   // TODO: Movable?
+   Instance(Instance&&) = delete;
+   Instance& operator=(Instance&&) = delete;
+
+   [[nodiscard]] operator VkInstance() const { return _instance; }
+   [[nodiscard]] VkInstance vk() const { return _instance; }
+
+   [[nodiscard]] const std::vector<std::shared_ptr<PhysicalDevice>>& get_physical_devices()
+       const;
+
+  private:
+   VkInstance _instance{VK_NULL_HANDLE};
+   std::vector<std::shared_ptr<PhysicalDevice>> _physical_devices{};
+};
+}  // namespace meddl::vk
