64f14d049dd82fbc7d0f3ed79d0f901bfe71e0b8
antsoh161 <soderlund.anton@gmail.com>
2024-03-17 02:42:47 +0100

    chore: Some project cleanups

--- include/wrappers/glfw/window.h
+++ include/wrappers/glfw/window.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <iostream>
 #include <memory>
 #include <string>
 
@@ -19,52 +18,31 @@ class Window {
   public:
    Window() noexcept = default;
 
-   Window(GLFWwindow* window) : window_handle(window, glfwDestroyWindow)
-   {
-   }
+   Window(GLFWwindow* window) : window_handle(window, glfwDestroyWindow) {}
 
-   Window(std::nullptr_t) noexcept : Window{}
-   {
-   }
+   Window(std::nullptr_t) noexcept : Window{} {}
 
-   Window(uint32_t width,
-          uint32_t height,
-          const std::string& title,
-          const Monitor* monitor = nullptr,
-          Window* share = nullptr)
-       : window_handle(glfwCreateWindow(static_cast<int>(width),
-                                        static_cast<int>(height),
-                                        title.c_str(),
-                                        nullptr,
-                                        nullptr),
+   Window(uint32_t width, uint32_t height, const std::string& title,
+          const Monitor* monitor = nullptr, Window* share = nullptr)
+       : window_handle(glfwCreateWindow(static_cast<int>(width), static_cast<int>(height),
+                                        title.c_str(), nullptr, nullptr),
                        glfwDestroyWindow)
    {
    }
 
-   ~Window() = default;  //=default;
+   ~Window() = default; 
 
    Window(const Window&) = delete;
-
    Window& operator=(const Window&) = delete;
 
    Window(Window&& other) noexcept = default;
-
    Window& operator=(Window&& other) noexcept = default;
 
-   operator GLFWwindow*() const
-   {
-      return window_handle.get();
-   }
+   operator GLFWwindow*() const { return window_handle.get(); }
 
-   void should_close(bool value)
-   {
-      glfwWindowShouldClose(window_handle.get());
-   }
+   void should_close(bool value) { glfwWindowShouldClose(window_handle.get()); }
 
-   void close()
-   {
-      glfwSetWindowShouldClose(window_handle.get(), true);
-   }
+   void close() { glfwSetWindowShouldClose(window_handle.get(), true); }
 
    [[nodiscard]] FrameBufferSize get_framebuffer_size() const
    {
