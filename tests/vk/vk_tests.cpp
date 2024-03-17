73fb402c264fc82a16505aafec330b5d031a79cf
antsoh161 <soderlund.anton@gmail.com>
2024-03-17 02:44:42 +0100

    feat: Add tests for initialization
    One test for one initialization function, since they all depend on
    eachother. None should throw, and if it does the log will show proper
    exception messages as to why.

---
+++ tests/vk/vk_tests.cpp
@@ -0,0 +1,98 @@
+#include <chrono>
+#include <print>
+#include <thread>
+
+#include "gtest/gtest.h"
+#include "vk/debug.h"
+#include "vk/defaults.h"
+#include "vk/device.h"
+#include "vk/instance.h"
+#include "vk/surface.h"
+#include "vk/swapchain.h"
+
+using namespace meddl::vk;
+using namespace meddl::glfw;
+
+class MeddlFixture : public ::testing::Test {
+  public:
+   void init_instance()
+   {
+      glfwInit();
+      glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+      glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+      auto app_info = meddl::vk::defaults::app_info();
+      auto debug_info = meddl::vk::defaults::debug_info();
+      _instance = std::make_unique<Instance>(app_info, debug_info, _debugger);
+   }
+   void init_window() { _window = std::make_shared<Window>(800, 600, "Test Window"); }
+   void init_surface() { _surface = std::make_unique<Surface>(_window.get(), _instance.get()); }
+   void init_device()
+   {
+      std::optional<int> present_index{};
+      std::optional<int> graphics_index{};
+
+      for (const auto& device : _instance->get_physical_devices()) {
+         present_index = device->get_present_family(_surface.get());
+         graphics_index = device->get_queue_family(VK_QUEUE_GRAPHICS_BIT);
+      }
+
+      auto physical_device = _instance->get_physical_devices().front();
+      auto extensions = meddl::vk::defaults::device_extensions();
+
+      auto config = QueueConfiguration(present_index.value());
+      std::unordered_map<uint32_t, QueueConfiguration> configs = {{graphics_index.value(), config}};
+      _device = std::make_unique<Device>(
+          physical_device.get(), configs, extensions, std::nullopt, _debugger);
+   }
+   void init_swapchain()
+   {
+      SwapchainOptions options{};
+      _swapchain = std::make_unique<Swapchain>(_instance->get_physical_devices().front().get(),
+                                                  _device.get(),
+                                                  _surface.get(),
+                                                  options,
+                                                  _window->get_framebuffer_size());
+   }
+   void init_all()
+   {
+      init_instance();
+      init_window();
+      init_surface();
+      init_device();
+      init_swapchain();
+   }
+   std::optional<Debugger> _debugger{};
+   std::unique_ptr<Instance> _instance{};
+   std::shared_ptr<Window> _window{};
+   std::unique_ptr<Surface> _surface{};
+   std::unique_ptr<Device> _device{};
+   std::unique_ptr<Swapchain> _swapchain{};
+};
+
+TEST_F(MeddlFixture, Initialization)
+{
+   EXPECT_NO_THROW(init_all());
+}
+
+//! @brief
+//! We require atleast the graphics bit, and present mode to be able to draw.
+// TEST_F(MeddlFixture, BareMinimumRequirements)
+// {
+//    init();
+//    EXPECT_GT(_instance->get_physical_devices().size(), 0);
+// }
+
+// TEST_F(MeddlFixture, TestDevice)
+// {
+//    init();
+//    auto physical_device = _instance->get_physical_devices().front();
+//    auto extensions = meddl::vk::defaults::device_extensions();
+//
+//    std::optional<int> present_index = physical_device->get_queue_family(VK_QUEUE_GRAPHICS_BIT);
+//    std::optional<int> graphics_index = physical_device->get_present_family(_surface.get());
+//    EXPECT_EQ(graphics_index.value(), present_index.value());
+//    auto config = QueueConfiguration(present_index.value());
+//    std::unordered_map<uint32_t, QueueConfiguration> configs = {{graphics_index.value(), config}};
+//    auto device = std::make_unique<NewDevice>(
+//        physical_device.get(), configs, extensions, std::nullopt, _debugger);
+// }
