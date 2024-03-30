5e765200c52162296730ca13d30f8ceb09743e34
antsoh161 <soderlund.anton@gmail.com>
2024-03-30 16:21:21 +0100

    feat: Add basic pipeline creation

--- include/vk/pipeline.h
+++ include/vk/pipeline.h
@@ -2,15 +2,47 @@
 
 #include "GLFW/glfw3.h"
 #include "vk/device.h"
-#include "shader.h"
+#include "vk/renderpass.h"
+#include "vk/shader.h"
 
 namespace meddl::vk {
 
+class PipelineLayout {
+public:
+   PipelineLayout(Device* device, VkPipelineLayoutCreateFlags flags);
+   ~PipelineLayout();
+
+   PipelineLayout(const PipelineLayout&) = delete;
+   PipelineLayout& operator=(const PipelineLayout&) = delete;
+
+   PipelineLayout& operator=(PipelineLayout&&) = default;
+   PipelineLayout(PipelineLayout&&) = default;
+
+   operator VkPipelineLayout() const { return _layout; }
+   [[nodiscard]] VkPipelineLayout vk() const { return _layout; }
+
+private:
+   Device* _device;
+   VkPipelineLayout _layout{VK_NULL_HANDLE};
+};
+
 class GraphicsPipeline {
   public:
-   GraphicsPipeline(const ShaderModule& vert_shader, const ShaderModule& frag_shader, Device* device);
+   GraphicsPipeline(const ShaderModule& vert_shader,
+                    const ShaderModule& frag_shader,
+                    Device* device,
+                    PipelineLayout* layout,
+                    RenderPass* render_pass);
+   ~GraphicsPipeline();
+
+   GraphicsPipeline(const GraphicsPipeline&) = delete;
+   GraphicsPipeline& operator=(const GraphicsPipeline&) = delete;
+
+   GraphicsPipeline(GraphicsPipeline&&) = default;
+   GraphicsPipeline& operator=(GraphicsPipeline&&) = default;
   private:
-   VkPipeline _pipeline;
+   PipelineLayout* _layout;
    Device* _device;
+   VkPipeline _pipeline;
 };
 }  // namespace meddl::vk
