b8a7add7847e6b3e7018fd60e94dc297a5dbf480
antsoh161 <soderlund.anton@gmail.com>
2025-03-14 22:35:34 +0100

    feat: add include_all header

---
+++ include/vk/vk.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "vk/async.h"
+#include "vk/command.h"
+#include "vk/debug.h"
+#include "vk/defaults.h"
+#include "vk/device.h"
+#include "vk/instance.h"
+#include "vk/pipeline.h"
+#include "vk/shader.h"
+#include "vk/surface.h"
+#include "vk/swapchain.h"
+#include "vk/error.h"
+#include "vk/surface.h"
+#include "vk/queue.h"
