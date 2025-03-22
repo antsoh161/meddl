6e66eec96a0ab57e3238a956e96a04249bca2ed9
antsoh161 <soderlund.anton@gmail.com>
2025-03-22 23:31:51 +0100

    feat: Add image wrapper
    Will be nice to have in the future although

---
+++ include/engine/render/vk/image.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <vulkan/vulkan_core.h>
+
+#include <optional>
+
+#include "engine/render/vk/device.h"
+#include "engine/render/vk/shared.h"
+namespace meddl::render::vk {
+
+class Image {
+  public:
+   ~Image();
+   Image(const Image&) = delete;
+   Image& operator=(const Image&) = delete;
+   Image(Image&& other) noexcept;
+   Image& operator=(Image&& other) noexcept;
+
+   //! Resources owned by this
+   static Image create(Device* device,
+                       const GraphicsConfiguration::AttachmentConfig& config,
+                       uint32_t width,
+                       uint32_t height);
+   //! This image does not own the handle or the memory
+   static Image create_deferred(VkImage image,
+                                Device* device,
+                                const GraphicsConfiguration::AttachmentConfig& config);
+
+   [[nodiscard]] VkImage vk() const { return _image; }
+   [[nodiscard]] VkImageView view() const { return _image_view; }
+   [[nodiscard]] VkDeviceMemory memory() const { return _memory; }
+   [[nodiscard]] bool is_owner() const { return _owned_resources.has_value(); }
+
+   void transition(VkCommandBuffer cmd_buffer,
+                   VkImageLayout old_layout,
+                   VkImageLayout new_layout,
+                   VkPipelineStageFlags src_stage = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+                   VkPipelineStageFlags dst_stage = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
+
+  private:
+   Image(Device* device, const GraphicsConfiguration::AttachmentConfig& config);
+   void populate_image_view();
+
+   struct Owned {
+      VkDeviceMemory memory;
+   };
+   Device* _device{VK_NULL_HANDLE};
+   GraphicsConfiguration::AttachmentConfig _config{};
+
+   VkImage _image{VK_NULL_HANDLE};
+   VkImageView _image_view{VK_NULL_HANDLE};
+   VkImageLayout _current_layout{VK_IMAGE_LAYOUT_UNDEFINED};
+   VkDeviceMemory _memory{VK_NULL_HANDLE};
+   std::optional<Owned> _owned_resources{std::nullopt};
+};
+}  // namespace meddl::render::vk
