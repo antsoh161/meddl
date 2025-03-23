5350acb157b1ac17c98436d6390846b67f91086c
antsoh161 <soderlund.anton@gmail.com>
2025-03-23 20:35:18 +0100

    refactor: Revamp error strategy/propagation

--- include/core/error.h
+++ include/core/error.h
@@ -1,65 +1,60 @@
 #pragma once
 
-#include <cstdint>
 #include <format>
-#include <system_error>
+#include <source_location>
 
 namespace meddl::error {
-
-class VulkanErrorCategory : public std::error_category {
+class Error {
   public:
-   [[nodiscard]] const char* name() const noexcept override { return "VulkanError"; }
-
-   [[nodiscard]] std::string message(int ev) const override;
-
-   static constexpr VulkanErrorCategory& get() noexcept
+   constexpr Error(std::string_view message,
+                   std::source_location location = std::source_location::current()) noexcept
+       : _message(message), _location(location)
    {
-      static VulkanErrorCategory instance;
-      return instance;
    }
-};
 
-enum class VulkanError : uint32_t {
-   CommandBufferNotReady,
-   CommandBufferNotRecording,
-   CommandBufferNotExecutable,
-   ShaderCompilationFailed,
-   ShaderFileReadError,
-   ShaderInvalidCompiler,
-};
-
-constexpr std::error_code make_error_code(VulkanError e) noexcept
-{
-   return {static_cast<int>(e), VulkanErrorCategory::get()};
-}
-
-struct ErrorInfo {
-   std::error_code code;
-   std::string details;
-
-   [[nodiscard]] constexpr inline std::string message() const
+   [[nodiscard]] constexpr std::string_view message() const noexcept { return _message; }
+   [[nodiscard]] constexpr const std::source_location& location() const noexcept
    {
-      if (details.empty()) {
-         return code.message();
-      }
-      return std::format("{}:{}: {}", code.value(), code.message(), details);
+      return _location;
    }
+
+   [[nodiscard]] std::string format() const
+   {
+      return std::format("{}({}): {}", _location.file_name(), _location.line(), _message);
+   }
+
+  private:
+   std::string_view _message;
+   std::source_location _location;
 };
 
 template <typename... Args>
-ErrorInfo make_error(std::error_code code, std::format_string<Args...> fmt, Args&&... args)
-{
-   return {code, std::format(fmt, std::forward<Args>(args)...)};
-}
+struct FormatString {
+   template <size_t N>
+   constexpr FormatString(const char (&fmt)[N]) : view(fmt)
+   {
+   }
+   std::string_view view;
+};
+
+namespace detail {
+template <typename... Args>
+struct FormattedError {
+   FormatString<Args...> fmt;
+   std::tuple<Args...> args;
+
+   [[nodiscard]] std::string message() const
+   {
+      return std::apply([this](const auto&... args) { return std::format(fmt.view, args...); },
+                        args);
+   }
+};
+}  // namespace detail
 
 template <typename... Args>
-ErrorInfo make_error(VulkanError code, std::format_string<Args...> fmt, Args&&... args)
+auto make_formatted_error(FormatString<Args...> fmt, Args&&... args)
 {
-   return {code, std::format(fmt, std::forward<Args>(args)...)};
+   return detail::FormattedError<Args...>{fmt, std::make_tuple(std::forward<Args>(args)...)};
 }
+
 }  // namespace meddl::error
-
-namespace std {
-template <>
-struct is_error_code_enum<meddl::error::VulkanError> : true_type {};
-}  // namespace std
