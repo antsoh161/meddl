0e15f7ac7955cb8d0c60c8039634d5fbe3c9e3f6
antsoh161 <soderlund.anton@gmail.com>
2025-04-04 22:33:15 +0200

    refactor: Use static create function in graphics pipeline

--- include/engine/render/vk/pipeline.h
+++ include/engine/render/vk/pipeline.h
@@ -2,6 +2,7 @@
 
 #include <vulkan/vulkan_core.h>
 
+#include "core/error.h"
 #include "engine/render/vk/descriptor.h"
 #include "engine/render/vk/device.h"
 #include "engine/render/vk/renderpass.h"
@@ -11,47 +12,51 @@ namespace meddl::render::vk {
 
 class PipelineLayout {
   public:
-   PipelineLayout(Device* device,
-                  const DescriptorSetLayout* dsl,
-                  VkPipelineLayoutCreateFlags flags = 0);
+   PipelineLayout() = default;
+   static std::expected<PipelineLayout, error::Error> create(Device* device,
+                                                             const DescriptorSetLayout* dsl,
+                                                             VkPipelineLayoutCreateFlags flags = 0);
+
    ~PipelineLayout();
 
    PipelineLayout(const PipelineLayout&) = delete;
    PipelineLayout& operator=(const PipelineLayout&) = delete;
 
-   PipelineLayout& operator=(PipelineLayout&&) = default;
-   PipelineLayout(PipelineLayout&&) = default;
+   PipelineLayout& operator=(PipelineLayout&&) noexcept;
+   PipelineLayout(PipelineLayout&&) noexcept;
 
    operator VkPipelineLayout() const { return _layout; }
    [[nodiscard]] VkPipelineLayout vk() const { return _layout; }
 
   private:
-   Device* _device;
+   Device* _device{nullptr};
    VkPipelineLayout _layout{VK_NULL_HANDLE};
 };
 
 class GraphicsPipeline {
   public:
-   GraphicsPipeline(ShaderModule* vert_shader,
-                    ShaderModule* frag_shader,
-                    Device* device,
-                    PipelineLayout* layout,
-                    RenderPass* render_pass,
-                    VkVertexInputBindingDescription binding_description,
-                    const std::array<VkVertexInputAttributeDescription, 4>& attribute_description);
+   GraphicsPipeline() = default;
+   static std::expected<GraphicsPipeline, error::Error> create(
+       ShaderModule* vert_shader,
+       ShaderModule* frag_shader,
+       Device* device,
+       PipelineLayout* layout,
+       RenderPass* render_pass,
+       VkVertexInputBindingDescription binding_description,
+       const std::array<VkVertexInputAttributeDescription, 4>& attribute_description);
    ~GraphicsPipeline();
 
    GraphicsPipeline(const GraphicsPipeline&) = delete;
    GraphicsPipeline& operator=(const GraphicsPipeline&) = delete;
 
-   GraphicsPipeline(GraphicsPipeline&&) = default;
-   GraphicsPipeline& operator=(GraphicsPipeline&&) = default;
+   GraphicsPipeline(GraphicsPipeline&&) noexcept;
+   GraphicsPipeline& operator=(GraphicsPipeline&&) noexcept;
 
    [[nodiscard]] VkPipeline vk() const { return _pipeline; }
 
   private:
-   PipelineLayout* _layout;
-   Device* _device;
+   PipelineLayout* _layout{nullptr};
+   Device* _device{nullptr};
    VkPipeline _pipeline{VK_NULL_HANDLE};
 };
 }  // namespace meddl::render::vk
